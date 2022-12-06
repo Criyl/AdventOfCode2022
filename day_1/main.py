86423ad1de6a261d1bbcdc0bda5fdacfca01f922
Christopher Carroll <c.carroll@obscuritylabs.com>
2022-12-06 18:26:42 -0500

    day 1 2/2

--- day_1/main.py
+++ day_1/main.py
@@ -1,13 +1,17 @@
 highest = 0
+arr = [0]
 with open("input.txt") as file:
     count = 0
     for line in file.readlines():
         stripped = line.strip()
         if stripped == '':
             highest = max(count, highest)
+            arr += [count]
             count = 0
         else:
             num = int(line.strip())
             count += num
 
-print(highest)
+print(f"Highest: {highest}")
+sumTopThree = sum(sorted(arr, reverse=True)[:3])
+print(f"Top Sum: {sumTopThree}")
