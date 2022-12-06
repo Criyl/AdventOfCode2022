492a4079d0b4abc1b1171dbc438421a5d879cd21
Christopher Carroll <c.carroll@obscuritylabs.com>
2022-12-06 18:23:47 -0500

    day 2 2/2

--- day_2/main.py
+++ day_2/main.py
@@ -1,22 +1,51 @@
-MATCHUP_SCORE = {
-    ('A', 'X'): 1+3,
-    ('A', 'Y'): 2+6,
-    ('A', 'Z'): 3+0,
-    ('B', 'X'): 1+0,
-    ('B', 'Y'): 2+3,
-    ('B', 'Z'): 3+6,
-    ('C', 'X'): 1+6,
-    ('C', 'Y'): 2+0,
-    ('C', 'Z'): 3+3
+XYZ_TO_ABC = {
+    'X': 'A',
+    'Y': 'B',
+    'Z': 'C'
 }
 
-total_score = 0
+MATCHUP_SCORE = {
+    ('A', 'A'): 1+3,
+    ('A', 'B'): 2+6,
+    ('A', 'C'): 3+0,
+    ('B', 'A'): 1+0,
+    ('B', 'B'): 2+3,
+    ('B', 'C'): 3+6,
+    ('C', 'A'): 1+6,
+    ('C', 'B'): 2+0,
+    ('C', 'C'): 3+3
+}
+
+XYZ_TO_RESULT = {
+    'X': 'L',
+    'Y': 'D',
+    'Z': 'W'
+}
+MATCHUP_VICTORY = {
+    ('A', 'D'): 'A',
+    ('A', 'W'): 'B',
+    ('A', 'L'): 'C',
+    ('B', 'L'): 'A',
+    ('B', 'D'): 'B',
+    ('B', 'W'): 'C',
+    ('C', 'W'): 'A',
+    ('C', 'L'): 'B',
+    ('C', 'D'): 'C'
+}
+
+part1 = 0
+part2 = 0
 with open("input.txt") as file:
     for line in file.readlines():
         if line == '':
             break
-
         opponent, cheater = line.strip().split(' ')
-        total_score += MATCHUP_SCORE[(opponent, cheater)]
 
-print(total_score)
+        part1 += MATCHUP_SCORE[(opponent, XYZ_TO_ABC[cheater])]
+    
+        desire = MATCHUP_VICTORY[(opponent, XYZ_TO_RESULT[cheater])]
+        part2 += MATCHUP_SCORE[(opponent, desire)]
+
+
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
