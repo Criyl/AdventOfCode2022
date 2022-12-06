a431f6b6fc7ff0ea323fd5a744db2ff15901256d
Christopher Carroll <c.carroll@obscuritylabs.com>
2022-12-06 17:52:50 -0500

    day 4 2/2

--- day_4/main.py
+++ day_4/main.py
@@ -5,20 +5,31 @@ def isRedundant(range_a, range_b):
     return set_a.issubset(set_b) or set_b.issubset(set_a)
 
 
+def doOverlap(range_a, range_b):
+    set_a = set(range_a)
+    set_b = set(range_b)
+    return len(set_a.intersection(set_b)) != 0
+
+
 def stringToRange(rangeString):
     split = rangeString.split('-')
     return range(int(split[0]), int(split[1])+1)
 
 
-count = 0
-with open("input.txt") as file:
-    for line in file.readlines():
-        a, b = line.strip().split(',')
+if __name__ == "__main__":
+    part1 = 0
+    part2 = 0 
+    with open("input.txt") as file:
+        for line in file.readlines():
+            a, b = line.strip().split(',')
 
-        range_a = stringToRange(a)
-        range_b = stringToRange(b)
+            range_a = stringToRange(a)
+            range_b = stringToRange(b)
 
-        redundant = isRedundant(range_a, range_b)
-        count += redundant
-        print(f" {a},{b} = {redundant}")
-print(count)
+            redundant = isRedundant(range_a, range_b)
+            overlap = doOverlap(range_a, range_b)
+            part1 += redundant
+            part2 += overlap
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
--- day_4/test_main.py
+++ day_4/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import isRedundant
+from main import isRedundant, doOverlap
 
 @pytest.mark.parametrize("range_a, range_b, expected", [
     (range(2, 4+1), range(6, 8+1), False),
@@ -11,3 +11,15 @@ from main import isRedundant
 ])
 def test_redundant(range_a, range_b, expected):
     assert isRedundant(range_a, range_b) == expected
+
+
+@pytest.mark.parametrize("range_a, range_b, expected", [
+    (range(2, 4+1), range(6, 8+1), False),
+    (range(2, 3+1), range(4, 5+1), False),
+    (range(5, 7+1), range(7, 9+1), True),
+    (range(2, 8+1), range(3, 7+1), True),
+    (range(6, 6+1), range(4, 6+1), True),
+    (range(2, 6+1), range(4, 8+1), True)
+])
+def test_overlap(range_a, range_b, expected):
+    assert doOverlap(range_a, range_b) == expected
