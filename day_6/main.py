c2f5f9e1eb90df237c39e355b71ee9870f96d2a2
Chris Carroll <Criyl@users.noreply.github.com>
2022-12-07 01:55:06 -0500

    day 6 challenge (#7)
    * day 6 1/2
    
    * day 6 2/2

---
+++ day_6/main.py
@@ -0,0 +1,25 @@
+
+def getMarker(signal, n=4):
+    sequence = []
+    index = 0
+
+    for c in signal:
+        sequence.append(c)
+        index += 1
+        if len(sequence) > n:
+            sequence = sequence[1:]
+        if len(set(sequence)) == n:
+            break
+
+    return index
+
+
+if __name__ == "__main__":
+    part1 = ""
+    part2 = ""
+    with open("day_6/input.txt") as file:
+        line = file.readline()
+        part1 = getMarker(line)
+        part2 = getMarker(line, 14)
+    print(f"Part1: {part1}")
+    print(f"Part2: {part2}")
