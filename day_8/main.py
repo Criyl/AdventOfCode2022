37134ac5d026c4d2fa8d6d837b1253486e74ab09
Chris Carroll <Criyl@users.noreply.github.com>
2022-12-08 02:19:55 -0500

    day 8 (#10)

---
+++ day_8/main.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+
+def gridify(text):
+    return [list(lineA) for lineA in text.splitlines()]
+
+
+def calculateVisibility(grid, choord):
+    x, y = choord
+    top = grid[x][y]
+
+    down = True
+    for i in range(x+1, len(grid)):
+        select = grid[i][y]
+        if select >= top:
+            down = False
+            break
+
+    up = True
+    for i in reversed(range(0, x)):
+        select = grid[i][y]
+        if select >= top:
+            up = False
+            break
+
+    right = True
+    for j in range(y+1, len(grid)):
+        select = grid[x][j]
+        if select >= top:
+            right = False
+            break
+
+    left = True
+    for j in reversed(range(0, y)):
+        select = grid[x][j]
+        if select >= top:
+            left = False
+            break
+
+    return left or right or up or down
+
+
+def visible(grid):
+    size = len(grid)
+    result = np.ones((size, size), bool).tolist()
+    for i in range(1, size-1):
+        for j in range(1, size-1):
+            result[i][j] = calculateVisibility(grid, (i, j))
+    return result
+
+
+def countGrid(grid):
+    count = 0
+    size = len(grid)
+    for i in range(0, size):
+        for j in range(0, size):
+            if grid[i][j]:
+                count += 1
+    return count
+
+
+def scenic(grid, choord):
+    x, y = choord
+    top = grid[x][y]
+
+    tally = 0
+    for i in range(x+1, len(grid)):
+        tally += 1
+        select = grid[i][y]
+        if select >= top or i == len(grid):
+            break
+    down = tally
+
+    tally = 0
+    for i in reversed(range(0, x)):
+        select = grid[i][y]
+        tally += 1
+        if select >= top or i == 0:
+            break
+    up = tally
+
+    tally = 0
+    for j in range(y+1, len(grid)):
+        select = grid[x][j]
+        tally += 1
+        if select >= top or j == len(grid):
+            break
+    right = tally
+
+    tally = 0
+    for j in reversed(range(0, y)):
+        select = grid[x][j]
+        tally += 1
+        if select >= top or j == 0:
+            break
+    left = tally
+
+    print(grid[x][y], (x, y), [up, left, right, down])
+    return left * up * right * down
+
+
+if __name__ == "__main__":
+    with open("day_8/input.txt") as file:
+        text = file.read()
+        grid = gridify(text)
+        vis = visible(grid)
+        part1 = countGrid(vis)
+        print(f"Part1: {part1}")
+
+        part2 = 0
+        part2 = 0
+        size = len(grid)
+        for i in range(0, size):
+            for j in range(0, size):
+                part2 = max(scenic(grid, (i, j)), part2)
+
+        print(f"Part2: {part2}")
---
+++ day_8/test_main.py
@@ -0,0 +1,69 @@
+import pytest
+
+from day_8 import gridify, visible, countGrid, scenic
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        (
+            "30373\n25512\n65332\n33549\n35390\n",
+            [
+                ["3", "0", "3", "7", "3"],
+                ["2", "5", "5", "1", "2"],
+                ["6", "5", "3", "3", "2"],
+                ["3", "3", "5", "4", "9"],
+                ["3", "5", "3", "9", "0"]],
+        )
+    ],
+)
+def test_gridify(text, expected):
+    assert gridify(text) == expected
+
+
+@pytest.mark.parametrize(
+    "grid, expected",
+    [
+        (
+            [
+                ["3", "0", "3", "7", "3"],
+                ["2", "5", "5", "1", "2"],
+                ["6", "5", "3", "3", "2"],
+                ["3", "3", "5", "4", "9"],
+                ["3", "5", "3", "9", "0"]
+            ],
+            [
+                [True, True, True, True, True],
+                [True, True, True, False, True],
+                [True, True, False, True, True],
+                [True, False, True, False, True],
+                [True, True, True, True, True]
+            ]
+        )
+    ],
+)
+def test_visible(grid, expected):
+    vis = visible(grid)
+    assert vis == expected
+    assert countGrid(vis) == 21
+
+
+grid = [
+            ["3", "0", "3", "7", "3"],
+            ["2", "5", "5", "1", "2"],
+            ["6", "5", "3", "3", "2"],
+            ["3", "3", "5", "4", "9"],
+            ["3", "5", "3", "9", "0"]
+        ]
+
+
+@pytest.mark.parametrize(
+    "choord, expected",
+    [
+        ((3, 2), 8),
+        ((1, 2), 4)
+    ],
+)
+def test_scenic(choord, expected):
+
+    assert scenic(grid, choord) == expected
